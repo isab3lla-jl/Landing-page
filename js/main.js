02a2a083a48ed4b6b93ec0dd64bc9c83b5412081
isab3lla-jl <isabella.juarezl111@gmail.com>
2024-10-20 19:34:06 -0600

    home section & navbar update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 
 
 $(document).ready(function() {
+    /*------- Navbar Shrink -------*/
+    $(window).on("scroll", function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else {
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
     /*------- Video Popup -------*/
     const videoSrc = $("#player-1").attr("src");
     $(".video-play-btn, .video-popup").on("click", function(){
