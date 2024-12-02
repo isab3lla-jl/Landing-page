3458e03107bb0d0009cbef7f48c0c5fd752eb1a1
isab3lla-jl <isabella.juarezl111@gmail.com>
2024-12-02 17:09:18 -0600

    team section

--- js/main.js
+++ js/main.js
@@ -78,4 +78,22 @@ $(document).ready(function() {
             }
         }
     });
+    /*------- team Carousel -------*/
+    $('.team-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
 });
